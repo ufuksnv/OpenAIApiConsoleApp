0ac75e09e521b47a3f7d903772f178431dbe0ec3
ufuk murat sınavu <u.murat0606@hotmail.com>
2024-02-24 15:16:53 +0300

    Add project files.

---
+++ OpenAIApiConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Azure;
+using Azure.AI.OpenAI;
+
+namespace OpenAIConsoleApp
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("Mesajınızı yazınız (en fazla 50 kelime):");
+
+            string prompt = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(prompt) || prompt.Length > 50)
+            {
+                Console.WriteLine("Hata: Mesaj boş olamaz veya en fazla 50 karakter içerebilir.");
+            }
+            else
+            {
+
+                OpenAIClient client = new("");
+                var chatCompletionsOptions = new ChatCompletionsOptions()
+                {
+                    DeploymentName = "gpt-3.5-turbo-0125",
+                    Messages =
+                      {
+                       new ChatRequestSystemMessage("Sen türk dil kurumunda çalışan bir asistansın ve görevin cümledeki yanlış kelimeleri düzeltmek."),
+
+                       new ChatRequestUserMessage(prompt),
+
+                      }
+                };
+
+                Response<ChatCompletions> response = await client.GetChatCompletionsAsync(chatCompletionsOptions);
+                ChatResponseMessage responseMessage = response.Value.Choices[0].Message;
+                Console.WriteLine($"[{responseMessage.Role.ToString().ToUpperInvariant()}]: {responseMessage.Content}");
+
+            }
+        }
+    }
+}
